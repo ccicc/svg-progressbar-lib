772f8b4db5bf905f18268fec5710e538d6778088
ccicc <codingli1992@gmail.com>
2019-04-17 20:10:30 +0800

    Auto-generated commit

--- svg-progressbar-lib/public-api.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/svg-progressbar-lib.service';
-export * from './lib/svg-progressbar-lib.component';
-export * from './lib/svg-progressbar-lib.module';
-export * from './lib/svg-progressbar-lib.config';
-export * from './lib/svg-progressbar-lib.ease';
