8d1c92b2d3f2a701173a9d6eb58b231d0ae0b7cc
ccicc <codingli1992@gmail.com>
2019-04-23 18:16:18 +0800

    update d3-joindata add text tween animation

--- projects/svg-progressbar-tester/src/app/pages/page-nav-views/d3-joindata/d3-joindata.component.ts
+++ projects/svg-progressbar-tester/src/app/pages/page-nav-views/d3-joindata/d3-joindata.component.ts
@@ -1,7 +1,19 @@
 /* tslint:disable variable-name */
 
 import { Component, OnInit, OnDestroy, ViewChild, ElementRef, Renderer2 } from '@angular/core';
-import { create, select, shuffle, Selection, transition, easeLinear } from 'd3';
+import {
+  active,
+  format,
+  create,
+  select,
+  shuffle,
+  Selection,
+  transition,
+  easeLinear,
+  easeExpOut,
+  interpolateNumber,
+  Transition
+} from 'd3';
 import { timer, Subscription } from 'rxjs';
 
 @Component({
@@ -13,9 +25,13 @@ export class D3JoindataComponent implements OnInit, OnDestroy {
   @ViewChild('svgContainer1') public svgContainer1: ElementRef;
   @ViewChild('svgContainer2') public svgContainer2: ElementRef;
   @ViewChild('svgContainer3') public svgContainer3: ElementRef;
-  private _g1: Selection<Element, undefined, null, undefined>;
-  private _g2: Selection<Element, undefined, null, undefined>;
-  private _g3: Selection<Element, undefined, null, undefined>;
+  @ViewChild('textContainer1') public textEle1: ElementRef;
+  @ViewChild('textContainer2') public textEle2: ElementRef;
+
+  private _g1: Selection<SVGGElement, undefined, SVGSVGElement, undefined>;
+  private _g2: Selection<SVGGElement, undefined, SVGSVGElement, undefined>;
+  private _g3: Selection<SVGGElement, undefined, SVGSVGElement, undefined>;
+
   private _alphabeta = 'abcdefghijklmnopqrstuvwxyz'.split('');
   private _unsubscription: Subscription;
 
@@ -36,6 +52,7 @@ export class D3JoindataComponent implements OnInit, OnDestroy {
     this._renderSvg1(this._alphabeta);
     this._renderSvg2(this._alphabeta);
     this._renderSvg3(this._alphabeta);
+    this._renderText();
 
     this._unsubscription = timer(2000, 2000).subscribe(_ => {
       this._renderSvg1(this._updateData());
@@ -59,7 +76,7 @@ export class D3JoindataComponent implements OnInit, OnDestroy {
       .attr('class', 'update')
       .attr('x', (data, index) => index * 30)
       .attr('dy', '.35em')
-      .merge(texts as Selection<any, any, any, any>)
+      .merge(texts as Selection<SVGTextElement, any, SVGGElement, any>)
       .text(data => data);
     texts.exit().remove();
   }
@@ -76,7 +93,7 @@ export class D3JoindataComponent implements OnInit, OnDestroy {
       .attr('class', 'update')
       .text(data => data)
       .attr('dy', '.35em')
-      .merge(texts as Selection<any, any, any, any>)
+      .merge(texts as Selection<SVGTextElement, any, SVGGElement, any>)
       .attr('x', (data, index) => index * 30);
   }
 
@@ -111,6 +128,41 @@ export class D3JoindataComponent implements OnInit, OnDestroy {
       .transition(t)
       .attr('y', 0)
       .attr('fill-opacity', 1);
+  }
+
+  private _renderText() {
+    const f = format(',d');
+    const t = transition().duration(2000).ease(easeExpOut);
+    this._textTransition1(f, t);
+    this._textTransition2(f, t);
+  }
+
+  private _textTransition1(f: any, t: Transition<Element, any, Element, any>) {
+    select(this.textEle1.nativeElement)
+      .select('p')
+      .transition(t)
+      .on('start', function repeat() {
+        active(this).tween('text', () => {
+          const selection = select(this);
+          const i = interpolateNumber(+selection.text().replace(/,/g, ''), Math.floor(Math.random() * 1e6));
+          return (time: number) => selection.text(f(i(time)));
+        }).transition().delay(2000).on('start', repeat);
+      });
+  }
+
+  private _textTransition2(f: any, t: Transition<Element, any, Element, any>) {
+    const wrapper = select(this.textEle2.nativeElement);
+    wrapper.select('p').transition(t).on('start', function repeat() {
+      active(this).style('opacity', 0).remove();
+      wrapper.append('p').style('opacity', 0)
+        .text(f(Math.floor(Math.random() * 1e6)))
+        .transition()
+        .duration(2000)
+        .style('opacity', 1)
+        .transition()
+        .delay(2000)
+        .on('start', repeat);
+    });
   }
 
   private _updateData() {
